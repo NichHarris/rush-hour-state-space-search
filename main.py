4879c69818a11877d96097a334103c778dabbadd
NichHarris <harris.nicholas1998@gmail.com>
2022-11-05 14:32:17 -0400

    Formating output

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ import argparse
 import json
 
 INPUT_FILE_PATH = 'input'
-OUTPUT_FILE_PATH = 'output'
+SOLUTIONS_PATH = 'output/solutions'
+SEARCH_PATH = 'output/search'
+
 HEIGHT = WIDTH = 6
 
 
-def format_output(fuels, grid):
+def format_solution(initial_board, runtime, search_path, solution_path_length, solution_path, method, id):
+    board = ' '.join(initial_board)
+    output_file = f'{SOLUTIONS_PATH}/{method}-sol-{id}.txt'
+    with open(output_file, 'w') as file:
+        file.write(f'Initial board oncfiguration: {board}')
+    return
+
+def format_grid(fuels, grid):
     for i, grid in enumerate(grid_list):
         flevels = f'Fuel levels for: {fuels[i]}' if len(fuels[i]) else 'All fuel levels 100'
         print(f'Case {i}: {flevels}')
@@ -50,6 +59,30 @@ if __name__ == '__main__':
         grid = [[test_case[0][i+(j*WIDTH)] for i in range(WIDTH)] for j in range(HEIGHT)]
         grid_list.append(grid)
 
-
-    format_output(fuels, grid_list)
+    format_grid(fuels, grid_list)
     print(fuels)
+
+    # output TODO
+    # For each grid:
+    #   For each search algorithm (UCS, GBFS, A*):
+        # output to two paths: the solution and the search
+        # for GBFS and A*: have each heuristic in a separate file
+    
+    # solution output file:
+        #Initial board
+        # board format
+
+        # lsit of fuel levels
+
+        # runtime
+        # search path length
+        #solution path moves
+        # solution path
+        # solution path breakdown, ex: A down 1 new_fuel_lvl  updated_grid car_fuel_lvl
+
+        # final fuel levels
+        # final board
+
+    # search output file:
+        # f(n) = ? g(n) = ? h(n) = ?, state = new board state
+
