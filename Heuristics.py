c7e4b528a258e1e47de5574bb40df1faf0a5791d
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2022-12-03 20:42:17 -0500

    Added check to keep heuristic admissible

--- Heuristics.py
+++ Heuristics.py
@@ -7,7 +7,8 @@
 #   - h1: Number of blocking vehicles
 #   - h2: Number of blocked positions
 #   - h3: Number of blocking vehicles * LAMBDA
-#   - h4: The number of blocked positions, when a blocking vehicle's fuel is 0, h = inf
+#   - h4: Number of blocked vehicles plus number of vehicles blocking each vertical vehicle in ambulance path
+#         Additional check of fuel level for blocking vehicles since no fuel for blocking vehicles implies no solution as path is blocked (h=inf)
 
 HEIGHT = WIDTH = 6
 GRID = HEIGHT * WIDTH
@@ -62,7 +63,9 @@ class Heuristics:
     def perform_h3(self):
         return self.perform_h1() * self.LAMBDA
 
-    # Heuristic 4: return the number of blocking cars, unless the car has no fuel remaining, return infinity
+    # Heuristic 4: Number of blocking vehicles plus min number of vehicles present above or below to unblock each blocking vehicle
+    # with an additional check for fuel level of 0
+    # Intention: Provides better estimate on the number of moves to unblock the blocking cars and clear the path for the ambulance
     def perform_h4(self):
         blocking_cars = 0
         prev_vehicle = '.'
@@ -90,17 +93,29 @@ class Heuristics:
 
                     # Check orientation of car and only add blocking if vertically oriented
                     if self.car_dict[car_letter][3] == 'v':
-                        # Search vertically for blocking cars 
-                        prev_vehicle = car_letter
-                        j =  i
-                        while j - WIDTH > 0:
-                            j -= WIDTH
-                            up_car = self.board[j]
-                            if up_car != prev_vehicle:
-                                if up_car != '.':
-                                    prev_block_up += 1
-                                prev_vehicle = up_car
+                        # Get vehicle size and index
+                        car_size, car_index = self.car_dict[car_letter][0], self.car_dict[car_letter][1]
+                        
+                        # Search upward for blocking cars
+                        # Note: Size must be at most 2 given only 2 rows above
+                        if car_size == 2:
+                            prev_vehicle = car_letter
+                            j =  i
+                            while j - WIDTH > 0:
+                                j -= WIDTH
+                                up_car = self.board[j]
+                                if up_car != prev_vehicle:
+                                    if up_car != '.':
+                                        prev_block_up += 1
 
+                                    prev_vehicle = up_car
+                        else:
+                            prev_block_up = float('inf')
+
+                        # Calculate number of spaces vehicle needs to move to unblock space
+                        unblock_space = 3 - car_index
+
+                        # Search downward for blocking cars
                         prev_vehicle = car_letter
                         j =  i
                         while j + WIDTH < WIDTH*HEIGHT:
@@ -109,6 +124,11 @@ class Heuristics:
                             if down_car != prev_vehicle:
                                 if down_car != '.':
                                     prev_block_down += 1
+
+                                unblock_space -= 1
+                                if unblock_space == 0:
+                                    break
+
                                 prev_vehicle = down_car
 
                         blocking_cars += min(prev_block_up, prev_block_down)
